c13e5c0e4989ac6d9d76d0b4e05be105ff7ee90d
Crunch Rock <lordpiraterock@outlook.com>
2021-01-03 15:21:38 -0500

    Initial commit. This codebase and development plan is a poor attempt sketched out late at night playing around with string generation that results in a memory error. Time to get to work.

---
+++ hasanbot.js
@@ -0,0 +1,136 @@
+const hasanTalk = `
+    when you looksmax and get the chad jaw surgery  
+    twitch chat is broken and i already miss you chat
+    GUYS IF ONE OF MY FANMADE UNAFFILIATED CHANNELS SAY THEYRE TAKING TWO WEEKS OFF THAT ISNT ME TAKING 2 WEEKS OFF WTF?! I HAVE NO CONTROL OVER WHAT THEY POST I JUST DONT MIND IF THEY WANT TO USE MY STREAMS TO MAKE THEIR OWN CONTENT.
+    I AM LIVE AND ALIVE. GET IN QUICK. WE'RE WATCHING A BODYBUILDER GO OVER MY BALDING CARE ROUTINE LMAO. SOME FUN COZY REACTS BEFORE RUST LATER.  
+    is there a different chat client than chatterino ? i'm done w this. shit is mega broken all the time.
+    when you wanna go scope a scene out in rust but then bloodthirsty fortnite zoomers are roaming the streets  
+     i play video games for one day on nye
+    
+    political peter’s in my audience    
+    fell asleep before 10 last night after checking my blood pressure. i am literally 700 years old.
+      looking at this fucked me up
+     remember when he tried to be overtly racist w the kamalamala thing and it sounded like a 14 year old trying to be bad?
+    Who thinks   should bring me into the world of rust tomorrow?
+    do it.   don’t be a coward. 100 meter dash against    
+    i’m sorry for being late everyone. i didn’t realize if i was an hour late for once the entire twitch platform would fall apart.
+     I worked to end Donald Trump’s presidency, you wrote a book hoping to profit from Trump being racist?
+    
+    So funny when immigration fell off the radar so your “deportations are good actually” hot take did too   
+    i watched a new years special tonight and i’ve decided, there are very few things worse than this.
+    lmao  
+    my mom fell asleep and cenk and i got lost in a convo about politics so we missed new years
+    or maybe i think it’s good to agitate both for and the dems at times since the democratic party is the only party w enough voters in our current duopoly. no ones stopping you from doing direct action or organizing outside of that my friend.
+    yes. is there something you’re not understanding about either tweet?
+    this 2k cash act battle in the senate wasnt just abt getting people immediate relief. it was also abt centering the ga runoffs on a policy that 78% of americans want, while showing the dangers of having a republican senate majority. you know, so ppl have a reason to vote democrat
+    jesus christ what a year
+          Somewhere in between  Dems are turncoats for not filibustering the NDAA  and  it's Repubs whatcha gonna do  is the idea that it would've been good politics to at least deny enough votes to force Perdue and Loeffler to leave the campaign trail in Georgia    
+     the republican strategy 
+    
+    if you're a corrupt, unlikable, out of touch, robotic multi millionaire elitist - just crank up the dial of racism, shoot some guns, claim you love god, wear a hunting vest, defend donald trump no matter how silly he is and these hogs will vote for you. 
+    kelly loeffler is rich republican white lady racist, not kkk racist. which is why i think her team deliberately placed her around the kkk dude to make her seem more georgia, she can just disavow later but the message will be sent. it's no different than her denim america flag hat
+    you can see days or weeks (8/21 lol) where i didn't stream since I first strapped on a camera on my ps4 and started broadcasting on twitch on 3/23/2018  
+    anyways its cheesy as fuck to say this but i always wanted a place for myself on the internet since i started doing on camera content at tyt, and i never really felt like i was able to build my own space there - and i'm really glad i found it at  
+    because of my community i was able to stream everyday for the past 365 days at an average of 10 hours a day. and it wasn't like a bet or a challenge, i literally noticed this a couple weeks ago after the election coverage. probably not the healthiest thing.  
+    2020 started with me leaving tyt &amp; going full time on twitch. it was chaotic with the primaries, losing fish during covid lockdowns, blm protests all the way to my first ever election coverage. I love the community i was able to build along the way. w/o them this wouldn't be fun
+    hell yeah brudder  
+    texans love to talk about having pride in their state and they end up voting for a melted candle wax debate club nerd like ted cruz, and a dude who puts tomato paste on his dried brisket like john cornyn. texan republicans are permanent cucks.
+    i will avenge my fallen brother  
+    REPUBLICANS ARE NOW TRYING TO ADD THE CORPORATE LIABILITY SHIELD BACK ON TO THE CASH ACT (2K ONLY) TO POISON IT. THEY WILL DO ANYTHING THEY CAN TO STOP YOU FROM GETTING 2K
+    it's wild to me that the tax cuts for the wealthy grow the economy party is going to this length to stop poor people getting 2k, by claiming that some people who don't need it might get it? reminder that this is your money that they're talking about btw.
+    IM LIVE AND ALIVE, GOING THROUGH THE NEWS QUICKLY THEN BACK TO RUST W/ EVERY STREAMER YOUTUBER ON THE PLANET WHERE I WILL LEARN HOW TO EXIST IN AN ANARCHO PRIMITIVIST SOCIETY.  
+        it’s the same reason why people like the lincoln project. people love hearing “the other side” confirm their worldview. except tim is pretty conservative in a lot of his pov so idk why they think he’s still progressive just because he worked at vice.
+    yes i am actually afraid of getting covid. my mom lives w me. i haven’t partied, traveled or seen most of my friends in months. why would i risk making my mom sick just to fly over and debate you, when we can safely do it over the internet? ridiculous.  
+        i am the poorest on the server prolly as poor as greek and xqc are tbh
+     The Lincoln Project raised $4.8 million between November 24th and December 16th hyping the Georgia Senate runoff elections. 
+    
+    Since then, it has spent $1.1 million on independent expenditures in those races and paid Steve Schmidt $1.5 million.     
+    i fought by your side and died valiantly to the chopper 3 times! impossible for me steal as i kept dying
+    i am thinking about rust and how i should build a better base.
+    okay but erdogan would whoop him
+    i’m sorry
+    okay rust is actually a lot of fun
+           How it started                 How it’s going
+    
+    #EnglishVirus   
+    i am playing anarcho primitivism simulator w every streamer rn. rust for the first time ever  
+    my updates acc has more clout than me wtf  
+           
+          Mitch McConnell is a heartless dried up ghoul. The only reason he is still alive is because he gathers life force from all Americans dying of poverty.
+      dude youre so fucking insanely stupid please stop. he's the president elect. the rest of the party is already doing the right things. all people should expect is for him to recognize their efforts. it's not even just bernie. it's pelosi and schumer as well.
+    democratic voters should remember that joe biden is not doing the bare minimum atm to win the ga runoff races. all he needs to do is mention mitch mcconnell and senate republicans as the sole obstructionists standing between americans and 2k in their time of need. disgusting.
+    imagine not using this opportunity to make a case for why people should be voting for democrats in the upcoming senate race in georgia.  
+    so far he has thanked republicans for passing the cares relief. and even chris christie for promoting mask wearing. still no mention of the cash act and republicans standing against it.
+    did no one tell joe biden that there's a ga senate runoff race centered around a HUGE winning issue for democrats? his conference is rehashing old talking points about his 100 day challenge where he'll ask people kindly to wear masks and shit. not a mention of 2k payments
+    WATCHING JOE BIDEN LIVE TALKING MALARKEY. LETS SEE IF HE CENTERS THE 2K ADDITIONAL PAYMENTS TO THE GEORGIA RACE AS IT HEATS UP.  
+    I AM LIVE. WATCHING REPUBLICAN SENATORS EXPLAIN WHY THE POORS DO NOT DESERVE 2K, DESPITE THE FACT THAT TRUMP AND THE REPUBLICAN BASE IS ON BOARD WITH THIS DIRECT CASH RELIEF.  
+    this is what was preventing me from starting the stream this morning.  
+      which keyboard stops my wife from leaving?
+    will be returning to the  90 day react-se  segments in the new year. promise.
+    the amish mafia absolutely slaps extremely excited for the new reactlord segments. now i need to find some amish to hang out with.
+      you should do a bring your balding reactionary to work day
+    if i made a killing simply sitting in front of a webcam talking shit (i do) id keep my mouth shut about the people who change the diapers on your covid denier dad and risk their own lives while doing so.
+    this is perhaps the most deranged pov i’ve ever seen from those who regularly contributed to covid becoming a much larger problem. it’s not the fucking frontline workers that got people to not take covid seriously.  
+     
+      king of the hill
+    top tier normie anime’s are cowboy bebop, samurai champloo and death note. maybe full metal alchemist. they’re all short. easy to follow, incredibly well dubbed and have minimal weird shit. then you can move on to other stuff.
+    nani! someone wants to dive into anime? they will probably love my favorite anime about a boy rabbit who gets his powers from groping schoolgirls and then he fights intergalactic crime w his older busty companion. just say something normal to get them into anime first.
+    anime fans literally can’t help themselves and adjust to the expectations of the person that’s diving into anime for the first time. ppl are replying to jim belushi asking about anime w wild shit like flat chested werewolf girl who’s nude for fun, just say cowboy bebop
+    hank hill is an incredible role model
+           We will be at Shatto Rec (3191 W 4th St in Ktown) tomorrow from 1-2 30 PM with  
+    
+    Supplies we are gathering  
+    Blankets 
+    Tents
+    Hats
+    Sleeping bags 
+    Socks 
+    Trash bags 
+    Sweatshirts
+    
+    If you can’t donate online then you can donate here      
+          Here's what Bernie Sanders and the Democrats are planning for the next few days in the Senate. If Mitch McConnell doesn't give them a floor vote on $2,000 checks, they can keep Senators in Washington through New Year's Day.  
+       
+    republicans are currently defending foreign aid in our military budget, while simultaneously shitting on $2k in direct payments to the american people.
+           That’s okay with me.
+    
+    Alternatively, if conservatives are suddenly so concerned w/ the cost of the bill, maybe they should focus on stripping their cushy tax giveaways for 3 martini lunches and thoroughbred racehorses instead of blocking critical support for working people.   
+    every republican argument against 2k in direct payments can be reduced to,  the slaves musn't know they can get more out of us, they will rebel in the future if you allow them to believe they deserve more. now get back to work. 
+    watching politicians in the house talk about how workers don't need $2000 but instead well wishes and to swiftly get back to work. come in only if you want your blood to boil  
+    DAYCARE IS NOW OPEN. TALKING ABOUT TRUMP SIGNING THE STIMMY, THE NYT N WORD APPLICATION STORY, NASHVILLE BOMBING AND THE DESIGNATION OF TERRORIST. TALKING ABT ALL THAT AND MORE. GET IN NOW.  
+    lots of people mistake religion for being anything but an ideology that can be wielded for good (christian abolitionists, liberation theology) or justify unimaginable evil acts. this video shows the problem is conservatism, not religion.  
+    the white dude who bombed nashville is a terrorist in our current colloquial understanding of the word. but the term itself is absolutely a political one that is wielded by those in power in an unconscious or conscious effort to create stereotypes which necessitate imperialism
+     i love these weird subreddits.
+    
+      
+    uni of tenessee knoxvile asking someone to withdraw their application because a video of them saying the n word when they were 15  is hilarious. there isn't a better brand fit for ut than that.
+    what was more disappointing, cyberpunk, belle delphine or wlr?
+      i got very excited
+    ITS RAINING LIKE A MF IN LA
+          goblin
+          stream starting later, I’ll be on   stream for chadvice
+    CHADVICE STARTING SOON W/ GUESTS LIKE  ,  ,   and   LETS GOOOO  
+    currently watching a 30k robot hurt locker a box van in tenessee wondering why it won't just shoot it.
+    i stretch for 20 mins every morning now. it’s helped my back and neck pain tremendously.
+    i am “i do neck exercises in the morning” years old.
+    sunday fundays are my favorite streams because they're cozy asf. I'm going to be playing resident evil 3 with the heart rate monitor later and ALSO CHADVICE RETURNS AT 4PM PACIFIC!!!!!!
+          i’m not anti immigration lmao, 1 billion americans would prolly ruin the planet tho (if our consumption remained the same)
+      let your nuts hang
+    gonna go back to king of the hill
+    im in a deep show / game hole rn it sucks
+    is watching wonder woman 84 just to be a part of the conversation worth it?
+    bugsnax is a fun game. short and sweet.
+    KINNA BUG KINNA SNAX  
+`
+
+
+const talk = (length) => {
+    let sentence = [];
+    while (length > 0) {
+        let rando = Math.floor(Math.random() * hasanTalk.length)
+        sentence.push(hasanTalk[rando])
+    }
+    console.log(sentence)
+}
+
+talk(8);
